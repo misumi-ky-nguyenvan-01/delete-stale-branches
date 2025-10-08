2f2ad3f1cf94238f35352b0aa58c7a8663da967e
kynv-3493 <nguyen.van.ky@sun-asterisk.com>
2025-10-08 13:14:57 +0900

    Refactor delete-stale-branches workflow inputs and update checkout action version

--- create-PR.sh
+++ create-PR.sh
@@ -1,3 +1,7 @@
 for branch in $(git for-each-ref --format='%(refname:short)' refs/remotes/origin/ | grep -v 'main$'); do
   gh pr create --head "${branch#origin/}" --base main --title "PR from ${branch#origin/}" --body "Auto-created PR from ${branch#origin/}"
 done
+
+# - Học được grep -v : loại bỏ những ai trùng và giữ những đứa còn lại
+# - học được 'main$' là so với cuối chuỗi nếu ko có $ thì là so với đầu chuỗi
+# - Học được Biểu thức ${branch#origin/} là một cú pháp "parameter expansion" (mở rộng tham số) trong Bash (shell scripting), dùng để cắt (xoá) một phần chuỗi từ biến.
